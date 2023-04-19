3198e873a90b22c55169b18b3d6f5353dc6a4280
Andres Vargas <avargas@applab.mx>
2023-04-19 00:45:42 -0600

    Cambio Componente TablaDinamica para usuarios

--- imuebles-nuxt/pages/usuario/index.vue
+++ imuebles-nuxt/pages/usuario/index.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script setup>
+//Se cambio esta vista para agregar los campos de tittles y tabla lo cual entregara al componente de tabla, este entregara la tabla de usuarios
 const titles = ['id', 'status', 'Nombre Usuario', 'Correo', 'Telefono', 'Fecha Inicio', 'Fecha Fin', 'Acciones'];
 const tabla = "usuario";
 useHead({
